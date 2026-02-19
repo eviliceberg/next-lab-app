a76c54833c4f44ccf0e2c8a6a4f8f5de3661eb3e
Artem Golovchenko <voiced.fair-0a@icloud.com>
2026-02-19 23:44:45 +0200

    feat: add theme toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
+'use client'
+
 import styles from './page.module.css'
 import Calculator from '@/app/Calculator'
+import { useState } from 'react'
 
 export default function Home() {
+    const [isDark, setIsDark] = useState<boolean>(true)
+
     return (
-        <div className={styles.page}>
+        <div
+            style={{
+                backgroundColor: isDark ? 'black' : 'lightgrey',
+                transition: 'all 0.3s ease',
+            }}
+            className={styles.page}
+        >
             <main className={styles.main}>
                 <Calculator />
             </main>
+            <button
+                className={styles.button}
+                onClick={() => setIsDark(!isDark)}
+            >
+                Toggle theme
+            </button>
         </div>
     )
 }
